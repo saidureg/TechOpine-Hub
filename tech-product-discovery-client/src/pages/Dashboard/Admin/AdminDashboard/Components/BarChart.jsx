d3d4631773cfa15817029540e0dd767abfbd0db7
saidureg <saidureg@gmail.com>
2024-06-05 00:15:38 +0600

    show LineChart and BarChart  in dashboard

--- tech-product-discovery-client/src/pages/Dashboard/Admin/AdminDashboard/Components/BarChart.jsx
+++ tech-product-discovery-client/src/pages/Dashboard/Admin/AdminDashboard/Components/BarChart.jsx
@@ -1,5 +1,67 @@
+import TitleCard from "../../../../../components/Cards/TitleCard";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 const BarChart = () => {
-  return <div>BarChart</div>;
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Subscriptions",
+        data: labels.map(() => {
+          return Math.random() * 1000 + 500;
+        }),
+        backgroundColor: "rgba(255, 99, 132, 1)",
+      },
+      {
+        label: "Ads",
+        data: labels.map(() => {
+          return Math.random() * 1000 + 500;
+        }),
+        backgroundColor: "rgba(53, 162, 235, 1)",
+      },
+    ],
+  };
+  return (
+    <TitleCard title={"Revenue"}>
+      <Bar options={options} data={data} />
+    </TitleCard>
+  );
 };
 
 export default BarChart;
